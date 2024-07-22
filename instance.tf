6f80b205a68787f772edb09d874a0345e27858e5
sonnaila venkatesham <sonnailavenkatesham95@gmail.com>
2024-07-22 16:41:05 +0530

    added files

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   count         = length(var.instance_names)
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
-  subnet_id     = var.instance_names == "web" ? aws_subnet.public.id : aws_subnet.private.id
+  subnet_id     = (var.instance_names[count.index] == "web" ? aws_subnet.public.id : aws_subnet.private.id)
   tags = {
     Name = var.instance_names[count.index]
   }
